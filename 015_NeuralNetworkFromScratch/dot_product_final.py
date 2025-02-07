325a9fbf6a0bc0bad185bbdf5d5f82773adc329f
MatthewKelseyRT <matthew.kelsey@rising-tide.ai>
2025-02-07 16:43:52 +0000

    Printing outputs

--- 015_NeuralNetworkFromScratch/dot_product_final.py
+++ 015_NeuralNetworkFromScratch/dot_product_final.py
@@ -9,10 +9,10 @@ w2 = [0.4, 1.8]
 # %% Question: which weight is more similar to input data X
 # Larger values of dot product indicate that the weight is more similar to the input data
 dot_X_w1 = X[0] * w1[0] + X[1] * w1[1]
-dot_X_w1
+print(dot_X_w1)
 # %%
 dot_X_w2 = X[0] * w2[0] + X[1] * w2[1]
-dot_X_w2
+print(dot_X_w2)
 # %%
 np.dot(X, w1)
 # %%
