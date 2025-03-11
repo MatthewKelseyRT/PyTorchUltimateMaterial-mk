f0a87e094865d2f1ddc0729fabb959a2a96c1d94
MatthewKelseyRT <matthew.kelsey@rising-tide.ai>
2025-03-11 09:13:45 +0000

    imports

--- 020_TensorIntro/Tensors.py
+++ 020_TensorIntro/Tensors.py
@@ -1,10 +1,10 @@
 # %% packages
 import numpy as np
 import seaborn as sns
-import torch
+from torch import Tensor, tensor
 
 # %% create a tensor
-x = torch.tensor(5.5)
+x: Tensor = tensor(5.5)
 
 # %% simple calculations
 y = x + 10
@@ -16,28 +16,28 @@ print(x.requires_grad)  # check if requires_grad is true, false if not directly 
 x.requires_grad_()  # set requires grad to true, default True
 
 # %% or set the flag directly during creation
-x = torch.tensor(2.0, requires_grad=True)
+x = tensor(2.0, requires_grad=True)
 print(x.requires_grad)
 
 
 # %% function for showing automatic gradient calculation
-def y_function(val):
+def y_function(val: float) -> float:
     return (val - 3) * (val - 6) * (val - 4)
 
 
-x_range = np.linspace(0, 10, 101)
-x_range
+x_range: np.ndarray = np.linspace(0, 10, 101)
+print(x_range)
 y_range = [y_function(i) for i in x_range]
 sns.lineplot(x=x_range, y=y_range)
 
 # %% define y as function of x
-y = (x - 3) * (x - 6) * (x - 4)
+y: Tensor = (x - 3) * (x - 6) * (x - 4)
 print(y)
 # %%
 
 # %% x -> y
 # create a tensor with gradients enabled
-x = torch.tensor(1.0, requires_grad=True)
+x = tensor(1.0, requires_grad=True)
 # create second tensor depending on first tensor
 y = (x - 3) * (x - 6) * (x - 4)
 # calculate gradients
@@ -45,7 +45,7 @@ y.backward()
 # show gradient of first tensor
 print(x.grad)
 # %% x -> y -> z
-x = torch.tensor(1.0, requires_grad=True)
+x = tensor(1.0, requires_grad=True)
 y = x**3
 z = 5 * y - 4
 
@@ -53,8 +53,8 @@ z = 5 * y - 4
 z.backward()
 print(x.grad)  # should be equal 5*3x**2
 # %% more complex network
-x11 = torch.tensor(2.0, requires_grad=True)
-x21 = torch.tensor(3.0, requires_grad=True)
+x11 = tensor(2.0, requires_grad=True)
+x21 = tensor(3.0, requires_grad=True)
 x12 = 5 * x11 - 3 * x21
 x22 = 2 * x11**2 + 2 * x21
 y = 4 * x12 + 3 * x22
